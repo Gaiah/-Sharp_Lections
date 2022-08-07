cb9845ee2e53d3227a54bb51bed245eb45ef3d61
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-07 11:47:28 +0400

    If-else example added

---
+++ examp005_/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yaay! It's Masha!!!");
+}
+
+else {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
